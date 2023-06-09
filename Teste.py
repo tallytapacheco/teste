899db633ab2bfc3fc6ba3b27a296475de0151622
tallytapacheco <thallyta11@hotmail.com>
2023-06-08 22:00:58 -0300

    atualizacao

---
+++ Teste.py
@@ -0,0 +1,14 @@
+x = 5
+y = 10.0
+print (x + y)
+z = x*y
+print (z)
+print (type(z))
+print("-"*20)
+pessoa = float (input ("Qual a sua altura?"))
+if pessoa > 2:
+    print ("a pessoa é alta")
+elif pessoa < 2 and pessoa >= 1.5:
+    print ("pessoa é normal")
+else:
+    print ("a pessoa é baixa")   
